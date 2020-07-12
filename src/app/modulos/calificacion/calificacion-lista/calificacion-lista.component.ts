94d368d7d9d2f1d7277688d38a328a45f6d75d7f
Raquel Toribio <anaraquel4895@gmail.com>
2020-07-12 17:12:52 -0400

    Creando Tablero de calificaciones

--- src/app/modulos/calificacion/calificacion-lista/calificacion-lista.component.ts
+++ src/app/modulos/calificacion/calificacion-lista/calificacion-lista.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-calificacion-lista',
@@ -7,9 +9,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalificacionListaComponent implements OnInit {
 
-  constructor() { }
+
+  displayedColumns = ['nombreCompleto', 'periodo', 'fecha', 'firstQuiz',
+    'secondQuiz', 'midterm', 'thirdQuiz', 'finalExam', 'nota', 'comentarios','opciones'];
+  dataSource = new MatTableDataSource<Inscrito>(ELEMENT_DATA);
+
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe(['(max-width: 600px)']).subscribe(result => {
+      this.displayedColumns = result.matches ?
+        ['nombreCompleto', 'periodo', 'fecha', 'firstQuiz',
+          'secondQuiz', 'midterm', 'thirdQuiz', 'finalExam', 'nota', 'comentarios', 'opciones'] :
+        ['nombreCompleto', 'periodo', 'fecha', 'firstQuiz',
+          'secondQuiz', 'midterm', 'thirdQuiz', 'finalExam', 'nota', 'comentarios', 'opciones'];
+    });
+  }
 
   ngOnInit(): void {
   }
 
+}
+
+const ELEMENT_DATA: Inscrito[] = [
+  {
+    nombreCompleto: 'Will Smith',
+    periodo: '',
+    fecha: '',
+    firstQuiz: 0,
+    secondQuiz: 0,
+    midterm: 0,
+    thirdQuiz: 0,
+    finalExam: 0,
+    nota: 0,
+    comentarios: ''
+  },
+  {
+    nombreCompleto: 'Johnny Depp',
+    periodo: 'Feb-20',
+    fecha: '23/02/2020',
+    firstQuiz: 30,
+    secondQuiz: 40,
+    midterm: 70,
+    thirdQuiz: 35,
+    finalExam: 60,
+    nota: 83,
+    comentarios: 'Well done'
+  },
+  {
+    nombreCompleto: 'Adam Sandler',
+    periodo: '',
+    fecha: '',
+    firstQuiz: 0,
+    secondQuiz: 0,
+    midterm: 0,
+    thirdQuiz: 0,
+    finalExam: 0,
+    nota: 0,
+    comentarios: ''
+  },
+];
+
+export class Inscrito {
+  nombreCompleto: string;
+  periodo: string;
+  fecha: string;
+  firstQuiz: number;
+  secondQuiz: number;
+  midterm: number;
+  thirdQuiz: number;
+  finalExam: number;
+  nota: number;
+  comentarios: string;
 }
