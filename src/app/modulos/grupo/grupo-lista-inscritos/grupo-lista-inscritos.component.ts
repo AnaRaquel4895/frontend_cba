695ff01493db896a57f8d4de293ed72c80748540
Raquel Toribio <anaraquel4895@gmail.com>
2020-07-19 00:11:46 -0400

    Cambios en la vista para alumnos inscritos a un grupo

--- src/app/modulos/grupo/grupo-lista-inscritos/grupo-lista-inscritos.component.ts
+++ src/app/modulos/grupo/grupo-lista-inscritos/grupo-lista-inscritos.component.ts
@@ -9,14 +9,14 @@ import { BreakpointObserver } from '@angular/cdk/layout';
 })
 export class GrupoListaInscritosComponent implements OnInit {
 
-  displayedColumns = ['position', 'name', 'weight', 'symbol', 'opciones'];
-  dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
+  displayedColumns = ['nombres', 'apellidoPaterno', 'apellidoMaterno', 'opciones'];
+  dataSource = new MatTableDataSource<Inscritos>(ELEMENT_DATA);
 
   constructor(breakpointObserver: BreakpointObserver) {
     breakpointObserver.observe(['(max-width: 600px)']).subscribe(result => {
       this.displayedColumns = result.matches ?
-        ['position', 'name', 'weight', 'symbol', 'opciones'] :
-        ['position', 'name', 'weight', 'symbol', 'opciones'];
+        ['nombres', 'apellidoPaterno', 'apellidoMaterno', 'opciones'] :
+        ['nombres', 'apellidoPaterno', 'apellidoMaterno', 'opciones'];
     });
   }
 
@@ -25,22 +25,18 @@ export class GrupoListaInscritosComponent implements OnInit {
 
 }
 
-export interface Element {
-  name: string; // N° grupo
-  position: string; // Curso
-  weight: string; // Horario
-  symbol: string; // Profesor
+export interface Inscritos {
+  nombres: string; // N° grupo
+  apellidoPaterno: string; // Curso
+  apellidoMaterno: string; // Horario
 }
 
-const ELEMENT_DATA: Element[] = [
-  { name: 'AIT 0A', position: '1', weight: '7:00-10:15', symbol: 'Charles Darwin' },
-  { name: 'ART 0A', position: '1', weight: '8:30-10:00', symbol: 'Napoleón Bonaparte' },
-  { name: 'AAI 1A', position: '1', weight: '13:45-17:00', symbol: 'Julio César' },
-  { name: 'AIT 0A', position: '2', weight: '7:00-10:15', symbol: 'Albert Einstein' },
-  { name: 'AI 7A', position: '1', weight: '18:35-21:45', symbol: 'George Washington' },
-  { name: 'AAR 3B', position: '1', weight: '8:30-10:00', symbol: 'William Shakespeare' },
-  { name: 'TRL 1B', position: '1', weight: '10:15-11:30', symbol: 'Cristóbal Colón' },
-  { name: 'TRL 6A', position: '1', weight: '07:00-08:30', symbol: 'Isaac Newton' },
-  { name: 'KDC 3B', position: '1', weight: '10:15-11:·30', symbol: 'Sigmund Freud' },
-  { name: 'PTSL 1B', position: '1', weight: '15:45:17:00', symbol: 'Martin Luther King' },
+const ELEMENT_DATA: Inscritos[] = [
+  { nombres: 'Jorge Luis', apellidoPaterno: 'Aramayo', apellidoMaterno: 'Almazan', },
+  { nombres: 'Ana Maria', apellidoPaterno: 'Rojas', apellidoMaterno: 'Rojas', },
+  { nombres: 'Ana Gisella', apellidoPaterno: 'Noguera', apellidoMaterno: 'Flores', },
+  { nombres: 'Pedro Armando', apellidoPaterno: 'Calizaya', apellidoMaterno: 'Aramayo', },
+  { nombres: 'Jose Maria', apellidoPaterno: 'Mamani', apellidoMaterno: 'Alba', },
+  { nombres: 'Ana Paola', apellidoPaterno: 'Gomez', apellidoMaterno: 'Fernandes', },
+  { nombres: 'Maria Jhanet', apellidoPaterno: 'Claros', apellidoMaterno: 'Cespedes', }
 ];
