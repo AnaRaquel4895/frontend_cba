0ff29a28f00a2e25d63c25c9c2babf65f5e031f1
Ana Raquel <anaraquel4895@gmail.com>
2020-08-17 17:23:08 -0400

    Modificaciones para los campos de los listados

--- src/app/modulos/programa/programa-lista/programa-lista.component.ts
+++ src/app/modulos/programa/programa-lista/programa-lista.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-programa-lista',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgramaListaComponent implements OnInit {
 
-  constructor() { }
+
+  displayedColumns = ['nombreCompleto', 'opciones'];
+  dataSource = new MatTableDataSource<any>([]);
+
+  constructor(public breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe(['(max-width: 600px)']).subscribe(result => {
+      this.displayedColumns = result.matches ?
+        ['nombreCompleto', 'opciones'] :
+        ['nombreCompleto', 'opciones'];
+    });
+  }
 
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<any>(ELEMENT_DATA);
+  }
+
+  editarCalificacion(): void {
   }
 
 }
+
+const ELEMENT_DATA: any[] = [
+  {
+    nombre: 'Will Smith',
+    descripcion: ''
+  },
+  {
+    nombre: 'Johnny Depp',
+    descripcion: ''
+  },
+  {
+    nombre: 'Adam Sandler',
+    descripcion: ''
+  },
+];
