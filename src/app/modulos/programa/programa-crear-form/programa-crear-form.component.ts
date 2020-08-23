3a53d6a26b6bc19b90a9a2d7c1880a026c4e574d
Ana Raquel <anaraquel4895@gmail.com>
2020-08-23 14:55:49 -0400

    Uniendo metodo crearprograma con backend

--- src/app/modulos/programa/programa-crear-form/programa-crear-form.component.ts
+++ src/app/modulos/programa/programa-crear-form/programa-crear-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ProgramaService } from '../services/programa.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-programa-crear-form',
@@ -10,22 +12,35 @@ export class ProgramaCrearFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private programaService: ProgramaService,
+    private router: Router) {
     this.form = fb.group({
+      id: [undefined],
       nombre: ['', [Validators.required]],
       descripcion: [undefined, []]
     });
-  }
-
-  get nombre(): FormControl {
-    return this.form.get('nombre') as FormControl;
   }
 
   ngOnInit(): void {
   }
 
   crear(): void {
-    console.log('Crear: ', this.form.value);
+    this.programaService.crear(this.form.value)
+      .subscribe(
+        (response) => {
+          this.router.navigate(['/programas/lista']);
+        }
+      );
+  }
+
+  get nombre(): FormControl {
+    return this.form.get('nombre') as FormControl;
+  }
+
+  get descripcion(): FormControl {
+    return this.form.get('descripcion') as FormControl;
   }
 
 }
