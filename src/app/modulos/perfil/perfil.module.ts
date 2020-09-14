ae845d4c849da02b652091ccd4dfed03e1a4779c
Raquel Toribio <anaraquel4895@gmail.com>
2020-09-13 22:18:48 -0400

    Generando componente para listar perfiles

--- src/app/modulos/perfil/perfil.module.ts
+++ src/app/modulos/perfil/perfil.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PerfilRoutingModule } from './perfil-routing.module';
+import { PerfilListaComponent } from './perfil-lista/perfil-lista.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PerfilListaComponent],
   imports: [
     CommonModule,
     PerfilRoutingModule
