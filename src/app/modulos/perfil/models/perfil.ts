d69f0569fc793c0cb990775bd4a8c8c4d7686d42
Raquel Toribio <anaraquel4895@gmail.com>
2020-09-13 22:10:47 -0400

    Generando Modulo, Servicio, Modelos para perfiles

---
+++ src/app/modulos/perfil/models/perfil.ts
@@ -0,0 +1,9 @@
+export interface Perfil {
+  id: number;
+  nombres: string;
+  apellido_paterno: string;
+  apellido_materno: string;
+  carnet_identidad: string;
+  celular: string;
+  user_id: number;
+}
---
+++ src/app/modulos/perfil/services/perfil.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Response } from '../../../models/response';
+import { Perfil } from '../models/perfil';
+
+const apiUrl = environment.apiUrl + '/perfiles-usuarios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PerfilService {
+
+  constructor(private http: HttpClient) { }
+
+  listar(roleId: number | string): Observable<Response<Perfil[]>> {
+    return this.http.get<Response<Perfil[]>>(`${apiUrl}/by-roles/${roleId}`);
+  }
+}
