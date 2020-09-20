3f59f40060d268a998ad08913f804cd8c21ef770
Ana Raquel <anaraquel4895@gmail.com>
2020-09-19 20:43:50 -0400

    Creando modelos y servicio para grupos

---
+++ src/app/modulos/grupo/models/grupo.ts
@@ -0,0 +1,9 @@
+export interface Grupo {
+  id: number;
+  perfil_usuario_id: number;
+  programa_id: number;
+  curso_id: number;
+  nivel_id: number;
+  horario_id: number;
+  gestion_id: number;
+}
---
+++ src/app/modulos/grupo/services/grupo.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { Response } from '../../../models/response';
+import { Grupo } from '../models/grupo';
+
+const apiUrl = environment.apiUrl + '/gestiones';
+@Injectable({
+  providedIn: 'root'
+})
+export class GrupoService {
+  constructor(private http: HttpClient) { }
+
+  crear(data: object): Observable<Response<Grupo>> {
+    return this.http.post<Response<Grupo>>(apiUrl, data);
+  }
+
+  listar(): Observable<Response<Grupo[]>> {
+    return this.http.get<Response<Grupo[]>>(apiUrl);
+  }
+
+}
